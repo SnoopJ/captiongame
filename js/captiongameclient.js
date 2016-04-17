0c3ef3f81f27985b4d7ddf1d13801d28ef2956eb
SnoopJeDi <snoopjedi@gmail.com>
2016-04-17 02:54:40 -0500

    authentication of words against freebies and hardcoded alloweds

--- js/captiongameclient.js
+++ js/captiongameclient.js
@@ -6,7 +6,7 @@ socket.on('gameStart', function(data) {
 });
 
 socket.on('nextRound', function(roundInfo) {
-  console.log(roundInfo);
+  //console.log(roundInfo);
   startRound(roundInfo.n,roundInfo.expiretime);
 });
 
@@ -16,7 +16,7 @@ socket.on('gameEnd', function(data) {
 });
 
 function doGame() {
-  socket.emit('startGame');
+  socket.emit('startGame')
 }
 
 function startTimer(duration) {
@@ -37,7 +37,7 @@ function startTimer(duration) {
 
 var roundids = ["word","image","vote"];
 function showRound(id) {
-  console.log(id)
+  //console.log(id)
   $(".roundContainer").hide();
   $("#"+id+"RoundContainer").fadeIn({duration: 300});
 }
@@ -48,3 +48,31 @@ function startRound(roundNumber,expireTime) {
     dt = expireTime>now ? expireTime - now : 0;
     startTimer(dt);
 }
+
+var freebies;
+$(function() {
+  $.getJSON({
+    url:"/static/freebies.json",
+    success: function(data) {
+      freebies = data.freebieWords;
+      console.log("freebie words:",freebies);
+    }
+  })
+
+  // extract button word
+  $("#imageRoundContainer").on('click','.btn',function() {
+    input = $("#userSentence");
+    word = $(this).text();
+    
+    if ( input.val().indexOf(word) < 0 ) {
+      // check if there's already a space with ternary
+      hasSpace = input.val().length == 0 || input.val().substr(-1,1) == " ";
+      input.val( input.val() + (hasSpace ? "" : " ") + word + " " );
+    }
+  });
+  $("#submitSentence").on('click',function(){
+    input = $("#userSentence");
+    console.log( "Submitting sentence:\n", input.val() );
+    socket.emit('sendSentence',{ sentence: input.val() });
+  });
+});
