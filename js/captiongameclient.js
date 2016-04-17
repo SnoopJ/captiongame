693752552236d98ee062741508511ef4c55d5102
SnoopJeDi <snoopjedi@gmail.com>
2016-04-16 19:07:02 -0500

    visual indication of communication on page

--- js/captiongameclient.js
+++ js/captiongameclient.js
@@ -1,13 +1,17 @@
 var socket = io();
 
-socket.on('gamestarted', function(data) {
-  console.log("game started!")
+socket.on('gameStart', function(data) {
+  $("<div>Game started!</div>").appendTo("body");
 });
 
-socket.on('nextround', function(roundInfo) {
-  console.log("going to next round, which is numbeR " + roundInfo.n)
+socket.on('nextRound', function(roundInfo) {
+  $("<div>Going to next round, which is number " + roundInfo.n +"</div>").appendTo("body");
 });
 
-socket.on('gameover', function(data) {
-  console.log("game over!");
+socket.on('gameEnd', function(data) {
+  $("<div>Game Over!</div>").appendTo("body");
 });
+
+function doGame() {
+  socket.emit('startGame');
+}
--- server.js
+++ server.js
@@ -18,41 +18,39 @@ io.on('connection', function(socket){
       console.log('client disconnected (id: ' + socket.id +' )');
     });
 
-    socket.on('gamestart', function(){
+    socket.on('startGame', function(){
       console.log("Received request to start a game cycle");
       game = new captionGame(socket);
-      // socket.emit('gamestarted');
-      // socket.emit('gameover');
-      // socket.emit('nextround',{n:3});
       game.startGame();
     })
 });
 
 captionGame = function(sock) {
     return {
+      numRounds: 3,
       currentRound : 1,
       socket: sock,
 
       startGame : function () {
         console.log("Starting game!");
-        this.socket.emit('gamestarted');
+        this.socket.emit('gameStart');
         var self = this;
         setTimeout(function() { self.nextRound() },1500);
       },
       nextRound : function () {
-        if (this.currentRound > 3) {
+        if (this.currentRound >= this.numRounds) {
           this.endGame();
         } else {
           this.currentRound += 1;
           console.log("Going to round " + this.currentRound);
-          this.socket.emit('nextround',{n: this.currentRound});
+          this.socket.emit('nextRound',{n: this.currentRound});
           var self = this;
           setTimeout(function() { self.nextRound() },1500);
         }
       },
       endGame : function () {
         console.log("Game over!");
-        this.socket.emit('gameover');
+        this.socket.emit('gameEnd');
       }
     };
 };
